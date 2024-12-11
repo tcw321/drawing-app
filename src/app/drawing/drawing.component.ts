caab9aea7738494ee8576722dba4b79c57a11e1d
Timothy C. Wright <tcw321@gmail.com>
2024-12-11 12:53:24 -0500

    some changes that did not work

--- src/app/drawing/drawing.component.ts
+++ src/app/drawing/drawing.component.ts
@@ -13,6 +13,7 @@ export class DrawingComponent implements OnInit {
   @ViewChild('drawingCanvas') canvas!: ElementRef<HTMLCanvasElement>;
   private context!: CanvasRenderingContext2D;
   private startPoint: { x: number; y: number } | null = null;
+  private lastImageData: ImageData | null = null;
   
   isDrawing = false;
   currentColor = '#000000';
@@ -30,44 +31,60 @@ export class DrawingComponent implements OnInit {
     const rect = this.canvas.nativeElement.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-
-    this.isDrawing = true;
-    this.context.beginPath();
-    this.context.moveTo(x, y);
-
+  
     if (this.drawingMode === 'line') {
-      this.startPoint = { x, y };
+      if (!this.startPoint) {
+        // First click - start the line
+        this.startPoint = { x, y };
+        // Save the current canvas state
+        this.lastImageData = this.context.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+      } else {
+        // Second click - finish the line
+        this.context.beginPath();
+        this.context.moveTo(this.startPoint.x, this.startPoint.y);
+        this.context.lineTo(x, y);
+        this.context.strokeStyle = this.currentColor;
+        this.context.stroke();
+        this.startPoint = null;
+        this.lastImageData = null;
+      }
+    } else {
+      // Freehand drawing
+      this.isDrawing = true;
+      this.context.beginPath();
+      this.context.moveTo(x, y);
     }
   }
-  
+ 
   draw(event: MouseEvent) {
-    if (!this.isDrawing) return;
-    
     const rect = this.canvas.nativeElement.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-
-    if (this.drawingMode === 'freehand') {
+  
+    if (this.drawingMode === 'freehand' && this.isDrawing) {
       this.context.lineTo(x, y);
+      this.context.strokeStyle = this.currentColor;
       this.context.stroke();
-    } else if (this.drawingMode === 'line' && this.startPoint) {
-      // Clear the canvas to remove the preview line
-      const imageData = this.context.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-      this.context.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-      this.context.putImageData(imageData, 0, 0);
-
+    } else if (this.drawingMode === 'line' && this.startPoint && this.lastImageData) {
+      // Restore the previous canvas state
+      this.context.putImageData(this.lastImageData, 0, 0);
+      
       // Draw the preview line
       this.context.beginPath();
       this.context.moveTo(this.startPoint.x, this.startPoint.y);
       this.context.lineTo(x, y);
+      this.context.strokeStyle = this.currentColor;
       this.context.stroke();
     }
   }
   
   stopDrawing() {
     this.isDrawing = false;
-    this.startPoint = null;
+    if (this.drawingMode === 'freehand') {
+      this.context.beginPath();
+    }
   }
+
   
   updateColor(event: Event) {
     const input = event.target as HTMLInputElement;
@@ -88,7 +105,9 @@ export class DrawingComponent implements OnInit {
   
   clearCanvas() {
     const canvas = this.canvas.nativeElement;
-    const context = canvas.getContext('2d')!;
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    this.context.clearRect(0, 0, canvas.width, canvas.height);
+    this.startPoint = null;
+    this.lastImageData = null;
   }
+  
 }
